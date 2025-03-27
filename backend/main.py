f54e68ff555fbdee8f8103251ef9c386756a4d2e
Layne Pitman <lpitman@ualberta.ca>
2025-03-27 11:38:52 -0600

    Put server on a loop to retry if it disconnects

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_cors import CORS
 from app.data_pipeline import data_pipeline
 from app.spotify_auth import spotify_auth
+import time
+import traceback
 
 app = Flask(__name__)
 
@@ -10,4 +12,10 @@ app.register_blueprint(spotify_auth)
 CORS(app)   # Allow frontend to connect
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    while True:
+        try:
+            app.run(host='0.0.0.0', port=5000, debug=True)
+        except Exception as e:
+            print("Server crashed or disconnected. Attempting restart in 5 seconds...")
+            traceback.print_exc()
+            time.sleep(5)
